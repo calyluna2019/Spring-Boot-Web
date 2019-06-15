84b953e4678ceb75d7a95e37fac52bbde3760976
calyluna87 <cgl_8791@hotmail.com>
2019-06-15 02:38:53 -0300

    metodo y @getmapping

--- src/main/java/com/bolsadeindeas/springboot/app/SpringBootWeb/Controller/IndexController.java
+++ src/main/java/com/bolsadeindeas/springboot/app/SpringBootWeb/Controller/IndexController.java
@@ -2,7 +2,14 @@ package com.bolsadeindeas.springboot.app.SpringBootWeb.Controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+
+    @GetMapping("/index")
+    public String index() {
+
+        return "index";
+    }
 }
