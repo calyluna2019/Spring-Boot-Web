cec89468f0ff570d80838d026584eb45ff89b580
calyluna87 <cgl_8791@hotmail.com>
2019-06-16 03:15:36 -0300

    @RequestMapping

--- src/main/java/com/bolsadeindeas/springboot/app/SpringBootWeb/Controller/IndexController.java
+++ src/main/java/com/bolsadeindeas/springboot/app/SpringBootWeb/Controller/IndexController.java
@@ -4,8 +4,10 @@ package com.bolsadeindeas.springboot.app.SpringBootWeb.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
     @GetMapping({"/index", "/", "/home"})
