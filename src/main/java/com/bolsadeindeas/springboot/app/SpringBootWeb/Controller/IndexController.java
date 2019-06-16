6a22346666a21b2a18ef1b4bedc34ef08b19d43b
calyluna87 <cgl_8791@hotmail.com>
2019-06-16 03:05:38 -0300

    Interface Model, ModelMap, ModelAndView

--- src/main/java/com/bolsadeindeas/springboot/app/SpringBootWeb/Controller/IndexController.java
+++ src/main/java/com/bolsadeindeas/springboot/app/SpringBootWeb/Controller/IndexController.java
@@ -2,13 +2,16 @@ package com.bolsadeindeas.springboot.app.SpringBootWeb.Controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/index")
-    public String index() {
+    @GetMapping({"/index", "/", "/home"})
+    public String index(Model model) {
+
+        model.addAttribute("Titulo", "Bienvenido a mi pagina con map");
 
         return "index";
     }
