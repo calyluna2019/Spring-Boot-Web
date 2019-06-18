cc66bd207aee65caf1672d4401674bbe2dedfb1c
calyluna87 <cgl_8791@hotmail.com>
2019-06-17 21:11:08 -0300

    agrego 2 paginas html5 y foreach

--- src/main/java/com/bolsadeindeas/springboot/app/SpringBootWeb/Controller/IndexController.java
+++ src/main/java/com/bolsadeindeas/springboot/app/SpringBootWeb/Controller/IndexController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/app")
@@ -23,13 +25,27 @@ public class IndexController {
     public String profile(Model model) {
 
         User user = new User();
+
         user.setName("Carlos");
         user.setLastName("Luna");
+        user.setEmail("cgl_8791@hotmail.com");
 
         model.addAttribute("User", user);
         model.addAttribute("Title", "profile of user: ".concat(user.getName()));
 
-
         return "profile";
+    }
+
+    @RequestMapping("/toList")
+    public String toList(Model model) {
+
+        List<User> users = Arrays.asList(new User("Carlos", "Luna", "cgl_8791@hotmail.com"),
+                new User("Alejandra", "Nieva", "djhdfj@hotmail.com"),
+                new User("Valentina", "Luna", "34kfgf@hotmail.com"));
+
+        model.addAttribute("Title", "list of users");
+        model.addAttribute("users", users);
+
+        return "toList";
     }
 }
--- src/main/java/com/bolsadeindeas/springboot/app/SpringBootWeb/Models/User.java
+++ src/main/java/com/bolsadeindeas/springboot/app/SpringBootWeb/Models/User.java
@@ -4,6 +4,16 @@ public class User {
 
     private String name;
     private String lastName;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String lastName, String email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
     public String getName() {
         return name;
@@ -19,5 +29,13 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
